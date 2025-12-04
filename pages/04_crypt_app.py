f222fe74550712f8e3a6ee7c2fa95fb15988e0b4
Kino <a81906@MacBook-Air.local>
2025-12-04 16:32:35 +0900

    バグ修正

--- pages/04_crypt_app.py
+++ pages/04_crypt_app.py
@@ -1,4 +1,3 @@
-import random
 from crypt.encrypt_utils import load_encrypted_csv, save_encrypted_csv
 
 import pandas as pd
@@ -9,19 +8,19 @@ import app.config as config
 from app.ui import get_fernet, render_top_image_base64
 
 
-def crypt_debug(header, raw_csv, encrypted_tmp_csv, encrypted_csv, tmp_csv, column_names):
+def crypt_debug(header, raw_csv, encrypted_tmp_csv, encrypted_csv, tmp_csv, column_names, keys):
     load_dotenv()
     st.subheader(header)
 
-    mode = st.radio("モードを選択", ["暗号化", "復号化"], key=random.random())
+    mode = st.radio("モードを選択", ["暗号化", "復号化"], key=keys[0])
     fernet = get_fernet()
     if mode == "暗号化":
-        if st.button("暗号化", key=random.random()):
+        if st.button("暗号化", key=keys[1]):
             df = pd.read_csv(raw_csv)
             save_encrypted_csv(encrypted_tmp_csv, df, fernet)
             st.success("暗号化しました！")
     elif mode == "復号化":
-        if st.button("復号化", key=random.random()):
+        if st.button("復号化", key=keys[2]):
             try:
                 df = load_encrypted_csv(encrypted_csv, fernet, columns=column_names)
                 df.to_csv(tmp_csv, index=False)
@@ -32,5 +31,5 @@ def crypt_debug(header, raw_csv, encrypted_tmp_csv, encrypted_csv, tmp_csv, colu
 
 render_top_image_base64(config.TOP_IMAGE_PATH2)
 st.title("🧑‍🔧 デバッグ用暗号化 / 復号化ツール")
-crypt_debug("内省用", config.SENTIMENT_CSV, config.ENCRYPT_SENTIMENT_TMP_CSV, config.ENCRYPT_SENTIMENT_CSV, config.SENTIMENT_TMP_CSV, ["日付", "対象", "事実", "感情", "詳細感情", "感想", "対処法"])
-crypt_debug("観察用", config.OBSERVATION_CSV, config.ENCRYPT_OBSERVATION_TMP_CSV, config.ENCRYPT_OBSERVATION_CSV, config.OBSERVATION_TMP_CSV, ["日付", "対象", "事実", "感情", "洞察", "対処法"])
+crypt_debug("内省用", config.SENTIMENT_CSV, config.ENCRYPT_SENTIMENT_TMP_CSV, config.ENCRYPT_SENTIMENT_CSV, config.SENTIMENT_TMP_CSV, ["日付", "対象", "事実", "感情", "詳細感情", "感想", "対処法"], [1, 2, 3])
+crypt_debug("観察用", config.OBSERVATION_CSV, config.ENCRYPT_OBSERVATION_TMP_CSV, config.ENCRYPT_OBSERVATION_CSV, config.OBSERVATION_TMP_CSV, ["日付", "対象", "事実", "感情", "洞察", "対処法"], [4, 5, 6])
