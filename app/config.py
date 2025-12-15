c8ed542641ac5d548d299842eca178408d84c396
Kino <a81906@MacBook-Air.local>
2025-12-15 11:01:07 +0900

    ログ整理

--- app/config.py
+++ app/config.py
@@ -6,25 +6,29 @@ MENTAL_CSV = "logs/mental_logs.csv"
 EXERCISE_CSV = "logs/exercise_logs.csv"
 SLEEP_CSV = "logs/sleep_logs.csv"
 CARE_CSV = "logs/care_logs.csv"
-ENCRYPT_SENTIMENT_CSV = "logs/encrypt_sentiment_logs.csv"
-ENCRYPT_OBSERVATION_CSV = "logs/encrypt_observation_logs.csv"
 FEEL_PATH = "logs/feelings.json"
 LOG_PATH = "logs/observation_logs.csv"
-EVERY_DAY_CHECK_PATH = "everyday_checklist.json"
+EVERY_DAY_CHECK_PATH = "logs/everyday_checklist.json"
 
-# for debug
-SENTIMENT_CSV = "logs/sentiment_logs.csv"
-OBSERVATION_CSV = "logs/observation_logs.csv"
-SENTIMENT_TMP_CSV = "logs/sentiment_logs_tmp.csv"
-OBSERVATION_TMP_CSV = "logs/observation_logs_tmp.csv"
-ENCRYPT_SENTIMENT_TMP_CSV = "logs/encrypt_sentiment_logs_tmp.csv"
-ENCRYPT_OBSERVATION_TMP_CSV = "logs/encrypt_observation_logs_tmp.csv"
+# crypt
+ENCRYPT_SENTIMENT_CSV = "logs/crypt/encrypt_sentiment_logs.csv"
+ENCRYPT_OBSERVATION_CSV = "logs/crypt/encrypt_observation_logs.csv"
+SENTIMENT_CSV = "logs/crypt/sentiment_logs.csv"
+OBSERVATION_CSV = "logs/crypt/observation_logs.csv"
+SENTIMENT_TMP_CSV = "logs/crypt/sentiment_logs_tmp.csv"
+OBSERVATION_TMP_CSV = "logs/crypt/observation_logs_tmp.csv"
+ENCRYPT_SENTIMENT_TMP_CSV = "logs/crypt/encrypt_sentiment_logs_tmp.csv"
+ENCRYPT_OBSERVATION_TMP_CSV = "logs/crypt/encrypt_observation_logs_tmp.csv"
 
-WEIGHT_CSV = "logs/weight.csv"
-HARDINESS_CSV = "logs/hardiness.csv"
-SELF_CARE_CSV = "logs/self_care.csv"
-HUMAN_SKILL_CSV = "logs/human_skill.csv"
+# constant
+WEIGHT_CSV = "logs/constant/weight.csv"
+HARDINESS_CSV = "logs/constant/hardiness.csv"
+SELF_CARE_CSV = "logs/constant/self_care.csv"
+HUMAN_SKILL_CSV = "logs/constant/human_skill.csv"
 
+
+
+# images
 TOP_IMAGE_PATH = "data/totoro.jpg"
 TOP_IMAGE_PATH1 = "data/igi.jpg"
 TOP_IMAGE_PATH2 = "data/freelen.jpg"
