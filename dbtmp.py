b1088388e75d3d74fe465a1d85ce84f23758e44c
Kino <a81906@MacBook-Air.local>
2026-07-01 23:34:17 +0900

    db作成初期

---
+++ dbtmp.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+conn = sqlite3.connect("data/task_rpg.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS task_logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date TEXT,
+    task_name TEXT,
+    done INTEGER
+)
+""")
+
+conn.commit()
+
+print("テーブル作成完了")
+cursor.execute("""
+SELECT name
+FROM sqlite_master
+WHERE type='table'
+""")
+
+print(cursor.fetchall())
+
+conn.commit()
+
+cursor.execute("""
+SELECT *
+FROM task_logs
+""")
+
+print(cursor.fetchall())
+
+import json
+
+with open("logs/daily_data.json", encoding="utf-8") as f:
+    data = json.load(f)
+
+print(type(data))
+
+from pprint import pprint
+
+first_day = next(iter(data))
+pprint(data[first_day])
+
+for date, day_data in data.items():
+
+    for task in day_data["tasks"]:
+
+        cursor.execute(
+            """
+            INSERT INTO task_logs
+            (date, task_name, done)
+            VALUES (?, ?, ?)
+            """,
+            (
+                date,
+                task["name"],
+                int(task["done"])
+            )
+        )
+
+conn.commit()
+
+cursor.execute("""
+SELECT COUNT(*)
+FROM task_logs
+""")
+
+print(cursor.fetchone())
